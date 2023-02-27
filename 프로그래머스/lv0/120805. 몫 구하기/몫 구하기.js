7a2ca5c46e587a536156c9c5c44c577245f58269
이경미 <105830217+GyeongmiLee@users.noreply.github.com>
2023-02-27 17:23:14 +0900

    Update 몫 구하기.js

--- 프로그래머스/lv0/120805. 몫 구하기/몫 구하기.js
+++ 프로그래머스/lv0/120805. 몫 구하기/몫 구하기.js
@@ -4,4 +4,7 @@ function solution(num1, num2) {
         return num;}
     let answer = hasValid(num1) / hasValid(num2);
     return Math.floor(answer);
+    //Math.floor() - 버림
+    //Math.ceil() - 올림
+    //Math.round() - 반올림
 }

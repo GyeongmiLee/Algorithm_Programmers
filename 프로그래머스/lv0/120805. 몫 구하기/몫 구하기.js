48bd8783f42645443f30e8de77ad851eefbcd5e5
이경미 <105830217+GyeongmiLee@users.noreply.github.com>
2023-02-27 17:22:19 +0900

    [level 0] Title: 몫 구하기, Time: 0.04 ms, Memory: 33.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120805. 몫 구하기/몫 구하기.js
@@ -0,0 +1,7 @@
+function solution(num1, num2) {
+    const hasValid = (num) => {
+        0 <= num && num <= 100 
+        return num;}
+    let answer = hasValid(num1) / hasValid(num2);
+    return Math.floor(answer);
+}

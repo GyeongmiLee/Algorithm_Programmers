04f4ce60c7cbaa3e7cf9d65f8dfc83f41f0a76ab
이경미 <105830217+GyeongmiLee@users.noreply.github.com>
2023-03-03 17:17:27 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -10,3 +10,18 @@ function solution(price) {
     }
     return price;
 }
+/* 인상깊었던 풀이
+const discounts = [
+    [500000, 20],
+    [300000, 10],
+    [100000,  5],
+]
+
+const solution = (price) => {
+    for (const discount of discounts)
+        if (price >= discount[0])
+            return Math.floor(price - price * discount[1] / 100)
+    return price
+}
+
+*/

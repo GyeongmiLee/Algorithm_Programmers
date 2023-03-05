127e2dcf4019a0a80a25c9b33d0d46ca9480b3ec
이경미 <105830217+GyeongmiLee@users.noreply.github.com>
2023-03-05 17:58:07 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -0,0 +1,10 @@
+function solution(my_string, n) {
+    //각 문자를 n만큼 반복하게
+    //split으로 나눠서 배열에 담고 for문 한번 더 돌려서(repaet써서 다시담기)
+    //string 개수만큼 for문 돌려서 answer에 repeat\
+    var answer = '';
+    for(let i = 0; i < my_string.length; i++){
+        answer += my_string[i].repeat(n)
+    }
+    return answer;
+}

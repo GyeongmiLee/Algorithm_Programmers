1ea7161605c50d626b15097a424127435fb65e03
이경미 <105830217+GyeongmiLee@users.noreply.github.com>
2023-03-05 16:29:46 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -0,0 +1,6 @@
+function solution(my_string) {
+    // split()함수: 문자열을 분할하는 함수, 특정 구분자를 기준으로 문자열을 나누어 배열로 반환
+    // join()함수: 배열의 모든 요소를 연결하여 하나의 문자열을 만든다.이때, 각 요소는 구분자로 구분됨.
+    let new_arr = my_string.split("").reverse().join("");
+    return new_arr
+}

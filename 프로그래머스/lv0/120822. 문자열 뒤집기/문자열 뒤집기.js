612faeba0a7ab0982cfbd8603c8137626d1aab3a
이경미 <105830217+GyeongmiLee@users.noreply.github.com>
2023-03-05 16:32:30 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -3,4 +3,6 @@ function solution(my_string) {
     // join()함수: 배열의 모든 요소를 연결하여 하나의 문자열을 만든다.이때, 각 요소는 구분자로 구분됨.
     let new_arr = my_string.split("").reverse().join("");
     return new_arr
+     // var answer = [...my_string].reverse().join("");
+    //spread문법 써도 됨!!
 }

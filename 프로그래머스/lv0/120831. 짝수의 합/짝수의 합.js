8495ca70595024e0d937a7ca42f6410e644fec42
이경미 <105830217+GyeongmiLee@users.noreply.github.com>
2023-03-07 00:08:59 +0900

    [level 0] Title: 짝수의 합, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    // forof문 + filter 짝수만 저장하고, reduce로 합산?
+    let arr = [];
+   for(let i = 0; i <= n ; i++){
+       if(i % 2 === 0){
+           arr.push(i)
+       }
+   }
+    return arr.reduce((a,b)=> a+b);
+}

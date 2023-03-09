a6e0a82b6ba7b519c300a63cf1e7191b4a956d88
이경미 <105830217+GyeongmiLee@users.noreply.github.com>
2023-03-09 23:56:41 +0900

    [level 0] Title: 가위 바위 보, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -6,3 +6,17 @@ function solution(rsp) {
     (rsp.split("")).forEach(a => {answer += a === "2" ? "0" : a === "0" ? "5": "2"})
     return answer;
 }
+
+/*
+function solution(rsp) {
+    let arr = {
+        2: 0,
+        0: 5,
+        5: 2
+    };
+    var answer = [...rsp].map(v => arr[v]).join("");
+    return answer;
+}
+
+
+*/

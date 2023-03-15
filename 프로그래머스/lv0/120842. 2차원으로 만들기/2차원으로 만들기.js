2ab8ee07afdcbc9f23579ea25ea0b51f0c3401b2
이경미 <105830217+GyeongmiLee@users.noreply.github.com>
2023-03-15 15:17:36 +0900

    [level 0] Title: 2차원으로 만들기, Time: 3.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -13,3 +13,25 @@ function solution(num_list, n) {
     }
     return answer;
 }
+
+/*
+
+function solution(num_list, n) {
+    var answer = [];
+
+    while(num_list.length) {
+        answer.push(num_list.splice(0,n));
+    //원본배열이 계속 삭제되니까 0번째부터 n번째까지 push 가능!
+    }
+
+    return answer;
+}
+
+ - 주목해야할 점
+ => slice: 원본배열을 그대로 보존해야하는 상황
+    slice(시작인덱스, 종료인덱스 전 인덱스)
+ => splice: 원본배열 건드림
+    splice(시작인덱스, 몇개의 값을 삭제할지, 추가할 값)
+    
+    https://www.daleseo.com/js-array-slice-splice/
+*/
